3f098276d527515e25f5d31314c7dc05c0546c70
alaarezk1989 <alaarezk1989@gmail.com>
2021-05-17 14:59:15 +0200

    service ingredients

--- firstProject/src/app/recipes/recipe.service.ts
+++ firstProject/src/app/recipes/recipe.service.ts
@@ -1,5 +1,7 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
@@ -7,17 +9,34 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('pasta', 'ala pasta with red souce', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/05/masala-pasta-500x500.jpg'),
-    new Recipe('pizaa', 'ala pizza with red souce', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/05/masala-pasta-500x500.jpg')
+    new Recipe('pasta',
+      'pasta with white sauce',
+      'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/05/masala-pasta-500x500.jpg',
+      [
+        new Ingredient('pasta', 300),
+        new Ingredient('milk', 50),
+        new Ingredient('mushroom', 2)
+      ]),
+    new Recipe('pizza',
+      'pizza with red sauce',
+      'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/05/masala-pasta-500x500.jpg',
+      [
+        new Ingredient('Flour', 500),
+        new Ingredient('Tomatoes', 10),
+        new Ingredient('meat', 1)
+      ])
   ];
 
+  constructor(private slService: ShoppingListService) {
+  }
   getRecipes() {
     return this.recipes.slice();
   }
 
-  constructor() {
-  }
 
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
   // tslint:disable-next-line:typedef
   setToActive(name: string) {
     console.log(name + ' = active');
