3103c95e36c55dae4bc0c1301d4189c54716af4b
alaarezk1989 <alaarezk1989@gmail.com>
2021-06-16 20:31:44 +0200

    subscription

--- firstProject/src/app/app.component.ts
+++ firstProject/src/app/app.component.ts
@@ -6,21 +6,17 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dinaFarms-angular';
-  oddNumbers: number[] = [];
-  evenNumbers: number[] = [];
+  // title = 'dinaFarms-angular';
+  // oddNumbers: number[] = [];
+  // evenNumbers: number[] = [];
+  // onIntervalFired(firedNumber: number) {
+  //   if (firedNumber % 2 === 0) {
+  //     this.evenNumbers.push(firedNumber);
+  //   } else {
+  //     this.oddNumbers.push(firedNumber);
+  //   }
+  // }
 
-  loadFeature = 'recipe';
 
-  onIntervalFired(firedNumber: number) {
-    if (firedNumber % 2 === 0) {
-      this.evenNumbers.push(firedNumber);
-    } else {
-      this.oddNumbers.push(firedNumber);
-    }
-  }
 
-  onNavigate(feature: string) {
-    this.loadFeature = feature;
-  }
 }
