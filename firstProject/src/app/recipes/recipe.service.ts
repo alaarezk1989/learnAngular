aa2dd6052e328537ed46c9b786719a4cd54fe54b
alaarezk1989 <alaarezk1989@gmail.com>
2021-06-22 17:37:47 +0200

    finishing form for recipe with reactive

--- firstProject/src/app/recipes/recipe.service.ts
+++ firstProject/src/app/recipes/recipe.service.ts
@@ -2,10 +2,12 @@ import {Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
 
+  recipeChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe('pasta',
@@ -42,6 +44,21 @@ export class RecipeService {
   // tslint:disable-next-line:typedef
   setToActive(name: string) {
     console.log(name + ' = active');
+  }
+
+  addRecipe(recipe: Recipe){
+    this.recipes.push(recipe);
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number,newRecipe: Recipe){
+    this.recipes[index]=newRecipe;
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number){
+    this.recipes.splice(index,1);
+    this.recipeChanged.next(this.recipes.slice());
   }
 
 }
